46dd84f4f184e4aed315e063c778757710a2b065
gaoweibao <gao.@qq.com>
2019-03-26 00:07:43 +0800

    完善入口文件，能够正常打开网页

--- main.js
+++ main.js
@@ -3,15 +3,21 @@ const path = require("path");
 const server = express();
 const ejs = require('ejs')
 
-server.use(express.static("statics"))
+server.use(express.static("src/statics"))
 
+server.set('views', path.resolve(__dirname, 'src/views'));
 server.set('view engine','ejs');
-people = ['geddy', 'neil', 'alex'],
-html = ejs.render('./src/views/login.html', {people: people});
 
-server.get("/",(req,res)=>{
-    res.send(html)
+server.get("/account/login",(req,res)=>{
+    res.render('login', {people: [1,2,3]});
 })
+server.get("/account/register",(req,res)=>{
+    res.render('register', {people: [1,2,3]});
+})
+
+// server.get("/studentmanager",(req,res)=>{
+//     res.render('login', {people: [1,2,3]});
+// })
 
 server.listen("1024",(err)=>{
     console.log(err)
--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require("path")
 const nodeExternals = require("webpack-node-externals")//忽略node_modules里面的模块，不进行打包
 const merge = require("webpack-merge")//合并配置文件
+// const CopyWebpackPlugin = require('copy-webpack-plugin')
 const config = require("./webpack.base")
 const servercongig={
     target:"node",//告诉webpack打包的是服务端的代码，一些node内置模块不需要打包
@@ -12,6 +13,17 @@ const servercongig={
     },
     externals:[
         nodeExternals()
-    ]
+    ],
+    // 避免打包之后__dirname跟__filename错误的问题
+    node: {
+        __filename: true,
+        __dirname: true
+    }
+    // plugins:[
+    //    new CopyWebpackPlugin(
+    //        [{ from: 'statics', to: 'src/statics' }],
+    //        [{ from: 'views', to: 'src/views' }]
+    //     )
+    // ]
 }
 module.exports=merge(config,servercongig)
